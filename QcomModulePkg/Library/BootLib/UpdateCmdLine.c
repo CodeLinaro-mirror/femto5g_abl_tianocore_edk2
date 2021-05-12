e6694490508903b1a5bdaf6b07f00361c7756980
guyang <guyang@qti.qualcomm.com>
2021-05-12 19:15:21 +0530

    QcomModulePkg: update the display commandline passed to kernel
    The change allows a method to update the display kernel commandline
    parameters passed to kernel.
    
    	fastboot oem display-cmdline <display_commandline_string>
    
    The above command store the display commandline string passed as argument.
    During the device bootup this string is appended with the display commandline
    passed from UEFI to ABL.
    
    Change-Id: I1e8ff8aa08cc260e2f211668350a75d367c306b0
    Signed-off-by: Guang Yang <guyang@qti.qualcomm.com>

--- QcomModulePkg/Library/BootLib/UpdateCmdLine.c
+++ QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -85,7 +85,7 @@ STATIC CHAR8 MacEthAddrBufCmdLine[MAX_IP_ADDR_BUF];
 STATIC CHAR8 *ResumeCmdLine = NULL;
 
 /* Display command line related structures */
-#define MAX_DISPLAY_CMD_LINE 256
+#define MAX_DISPLAY_CMD_LINE (256 + MAX_DISPLAY_CMDLINE_LEN)
 STATIC CHAR8 DisplayCmdLine[MAX_DISPLAY_CMD_LINE];
 STATIC UINTN DisplayCmdLineLen = sizeof (DisplayCmdLine);
 
@@ -296,6 +296,8 @@ TargetBatterySocOk (UINT32 *BatteryVoltage)
 STATIC VOID GetDisplayCmdline (VOID)
 {
   EFI_STATUS Status;
+  CHAR8 *Src = NULL;
+  UINT32 SrcLen = 0;
 
   Status = gRT->GetVariable ((CHAR16 *)L"DisplayPanelConfiguration",
                              &gQcomTokenSpaceGuid, NULL, &DisplayCmdLineLen,
@@ -304,6 +306,13 @@ STATIC VOID GetDisplayCmdline (VOID)
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Unable to get Panel Config, %r\n", Status));
   }
+
+  Status = ReadDisplayCmdLine (&Src, &SrcLen);
+  if (Status != EFI_SUCCESS) {
+    DEBUG ((EFI_D_ERROR, "Unable to read display cmdline, %r\n", Status));
+  }
+
+  AsciiStrCatS (DisplayCmdLine, DisplayCmdLineLen, Src);
 }
 
 /*
