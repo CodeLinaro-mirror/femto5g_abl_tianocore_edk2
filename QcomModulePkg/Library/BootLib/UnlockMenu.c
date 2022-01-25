4d749c020756e2b8a6eb7973cf05147c2981c36e
Sairam Bandikanti <quic_sbandika@quicinc.com>
2022-01-25 15:21:30 +0530

    QcomModulePkg : Add enable OEM lock for wearable target
    Optimized menu configuration for wearable target by adding target specific flag.
    
    1) The smallest font size supported by UEFI is 8x19 and need compute 
    the rows/columns of the text console accordingly.
    
    2) No smaller font size available in UEFI, removed the extra space lines.
    
    Change-Id: I842ca82ffa6620977e699fb1063bd1fbccc933fb

--- QcomModulePkg/Library/BootLib/UnlockMenu.c
+++ QcomModulePkg/Library/BootLib/UnlockMenu.c
@@ -25,6 +25,38 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following
+ * license:
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided ?with the distribution.
+ *  * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED
+ * BY THIS LICENSE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "AutoGen.h"
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
@@ -126,7 +158,72 @@ STATIC MENU_MSG_INFO mUnlockMenuMsgInfo[] = {
      NOACTION},
 };
 
-STATIC MENU_MSG_INFO mLockMenuMsgInfo[] = {
+#ifdef WEAR_OS
+STATIC MENU_MSG_INFO LockMenuMsgInfo[] = {
+    {{"<!>"}, BIG_FACTOR, BGR_RED, BGR_BLACK, COMMON, 0, NOACTION},
+    {{"If you lock the bootloader, you will not be able to install "
+      "custom operating system on this phone.\n"},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     COMMON,
+     0,
+     NOACTION},
+    {{"To prevent unauthorized access to your personal data,"
+      "locking the bootloader will also delete all personal "
+      "data on your phone."},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     COMMON,
+     0,
+     NOACTION},
+    {{"\nPress the Volume keys to select whether to "
+      "unlock the bootloader, then the power button to continue."},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     COMMON,
+     0,
+     NOACTION},
+    {{"__________"},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     LINEATION,
+     0,
+     NOACTION},
+    {{"DO NOT LOCK THE BOOTLOADER"},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     OPTION_ITEM,
+     0,
+     RESTART},
+    {{"__________"},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     LINEATION,
+     0,
+     NOACTION},
+    {{"LOCK THE BOOTLOADER"},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     OPTION_ITEM,
+     0,
+     RECOVER},
+    {{"__________"},
+     COMMON_FACTOR,
+     BGR_WHITE,
+     BGR_BLACK,
+     LINEATION,
+     0,
+     NOACTION},
+};
+#else
+STATIC MENU_MSG_INFO LockMenuMsgInfo[] = {
     {{"<!>"}, BIG_FACTOR, BGR_RED, BGR_BLACK, COMMON, 0, NOACTION},
     {{"\n\nIf you lock the bootloader, you will not be able to install "
       "custom operating system on this phone.\n"},
@@ -189,7 +286,7 @@ STATIC MENU_MSG_INFO mLockMenuMsgInfo[] = {
      0,
      NOACTION},
 };
-
+#endif
 /**
   Reset device unlock status
   @param[in] Type    The type of the unlock.
@@ -254,8 +351,8 @@ UnlockMenuShowScreen (OPTION_MENU_INFO *OptionMenuInfo,
     OptionMenuInfo->Info.MsgInfo = mUnlockMenuMsgInfo;
     MaxLine = ARRAY_SIZE (mUnlockMenuMsgInfo);
   } else {
-    OptionMenuInfo->Info.MsgInfo = mLockMenuMsgInfo;
-    MaxLine = ARRAY_SIZE (mLockMenuMsgInfo);
+    OptionMenuInfo->Info.MsgInfo = LockMenuMsgInfo;
+    MaxLine = ARRAY_SIZE (LockMenuMsgInfo);
   }
 
   for (i = 0; i < MaxLine; i++) {
