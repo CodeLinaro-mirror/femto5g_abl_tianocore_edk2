f546850099fd309ad04ad542d14e2c1facdaedf8
Lijuan Gao <quic_lijuang@quicinc.com>
2022-03-01 21:05:36 +0800

    QcomModulePkg: Perform version check before using ThreadGetUnsafeSPBase
    Perform version check in BootLinux() before making use of
    KernIntf->Thread->ThreadGetUnsafeSPBase since this callback is not
    implemented for older version of the protocol and using it results
    in crash.
    
    Change-Id: I026d9c6d9f987bf03013ddbc8e7f9f5940a76495

--- QcomModulePkg/Library/BootLib/BootLinux.c
+++ QcomModulePkg/Library/BootLib/BootLinux.c
@@ -30,6 +30,38 @@
  *
  */
 
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following
+ * license:
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided ?with the distribution.
+ *  * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED
+ * BY THIS LICENSE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <Library/DeviceInfo.h>
 #include <Library/DrawUI.h>
 #include <Library/PartitionTableUpdate.h>
@@ -1036,9 +1068,9 @@ BootLinux (BootInfo *Info)
   BootParamlist BootParamlistPtr = {0};
 
   EFI_KERNEL_PROTOCOL *KernIntf = NULL;
-  Thread *ThreadNum;
-  VOID *StackBase;
-  VOID **StackCurrent;
+  Thread *ThreadNum = NULL;
+  VOID *StackBase = NULL;
+  VOID **StackCurrent = NULL;
 
   if (Info == NULL) {
     DEBUG ((EFI_D_ERROR, "BootLinux: invalid parameter Info\n"));
@@ -1213,9 +1245,11 @@ BootLinux (BootInfo *Info)
     goto Exit;
   }
 
-  ThreadNum = KernIntf->Thread->GetCurrentThread ();
-  StackBase = KernIntf->Thread->ThreadGetUnsafeSPBase (ThreadNum);
-  StackCurrent = KernIntf->Thread->ThreadGetUnsafeSPCurrent (ThreadNum);
+  if (KernIntf->Version >= EFI_KERNEL_PROTOCOL_VER_UNSAFE_STACK_APIS) {
+    ThreadNum = KernIntf->Thread->GetCurrentThread ();
+    StackBase = KernIntf->Thread->ThreadGetUnsafeSPBase (ThreadNum);
+    StackCurrent = KernIntf->Thread->ThreadGetUnsafeSPCurrent (ThreadNum);
+  }
 
   DataSize = sizeof (KernelSizeReserved);
   Status = gRT->GetVariable ((CHAR16 *)L"KernelSize", &gQcomTokenSpaceGuid,
