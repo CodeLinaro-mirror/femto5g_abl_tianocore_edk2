f5b891b08bb35b3ea2b695bce72ef71d0799550b
Sandeepkumar Yenugula <quic_syenugul@quicinc.com>
2022-03-09 13:03:32 +0530

    QcomModulePkg: correcting calculation of getting boot image size
    Correcting logic for calculating boot.img size by using offset
    for recovery_dtbo_size and dtb_size.
    
    Change-Id: Iae020feca84be376ed8c0a7220b00e43751746d0

--- QcomModulePkg/Library/avb/libavb/avb_ops.c
+++ QcomModulePkg/Library/avb/libavb/avb_ops.c
@@ -49,7 +49,40 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
+/* Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
 #include "Board.h"
 #include "BootImage.h"
 #include "BootLinux.h"
@@ -650,7 +683,8 @@ AvbIOResult AvbGetActualSizeOfBootImage(AvbOps *Ops,
 
 		if (HeaderVersion != BOOT_HEADER_VERSION_ZERO) {
 			ADD_TO_SIZE(*OutSizeNumBytes, PageSize,
-						((struct boot_img_hdr_v1 *)(ImageHdrBuffer))->recovery_dtbo_size);
+			((struct boot_img_hdr_v1 *)(ImageHdrBuffer +
+			sizeof(boot_img_hdr)))->recovery_dtbo_size);
 			HeaderSize = ((struct boot_img_hdr_v1 *)(ImageHdrBuffer))->header_size;
 		}
 
@@ -658,7 +692,10 @@ AvbIOResult AvbGetActualSizeOfBootImage(AvbOps *Ops,
 
 		if (HeaderVersion == BOOT_HEADER_VERSION_TWO) {
 			ADD_TO_SIZE(*OutSizeNumBytes, PageSize,
-								((struct boot_img_hdr_v2 *)(ImageHdrBuffer))->dtb_size);
+								((struct boot_img_hdr_v2 *)
+								((UINT64)ImageHdrBuffer +
+						BOOT_IMAGE_HEADER_V1_RECOVERY_DTBO_SIZE_OFFSET +
+						BOOT_IMAGE_HEADER_V2_OFFSET))->dtb_size);
 		}
 	} else if (HeaderVersion == BOOT_HEADER_VERSION_THREE) {
 		if (!AsciiStrnCmp (PARTITION, "boot", AsciiStrLen ("boot")) ||
