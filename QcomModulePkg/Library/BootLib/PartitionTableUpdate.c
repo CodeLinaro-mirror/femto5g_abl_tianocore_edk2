7cdda3589514aa9ff3aa0d2e0d2201704c1eb632
Madhu Ananthula <quic_mananthu@quicinc.com>
2022-04-27 22:22:08 +0530

    QcomModulePkg: Fix for potential Buffer Overflow
    Noted Buffer Overflow - Array Index Out of Bounds in
    in avb and bootlib modules.
    
    Change-Id: Ic6c5c05c1021bc0cf32ae50e2651e5326d809c41
    Signed-off-by: Madhu Ananthula <quic_mananthu@quicinc.com>

--- QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
+++ QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
@@ -25,6 +25,40 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 #include "PartitionTableUpdate.h"
 #include "AutoGen.h"
@@ -1841,8 +1875,8 @@ EFI_STATUS NandABUpdatePartition (UINT32 UpdateType)
   EFI_GUID Ptype = gEfiMiscPartitionGuid;
   EFI_STATUS Status;
   UINT32 PageSize;
-  size_t Size1 = sizeof (PtnEntries[0].PartEntry.PartitionName);
-  size_t Size2 = sizeof (NandAttr->Slots[0].SlotName);
+  size_t Size1 = ARRAY_SIZE (PtnEntries[0].PartEntry.PartitionName);
+  size_t Size2 = ARRAY_SIZE (NandAttr->Slots[0].SlotName);
 
   GetPageSize (&PageSize);
   Status = GetNandMiscPartiGuid (&Ptype);
