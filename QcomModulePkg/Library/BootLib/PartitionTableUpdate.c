a01f49ff7002eab7be7f3126c90609e4ab70d293
Lijuan Gao <quic_lijuang@quicinc.com>
2022-10-12 15:48:54 +0800

    QcomModulePkg: Fixed the possible security issue when getting partition size
    Fixed the possible security issue when getting partition size.
    1. Interger overflow while adding LastBlock and 1.
    2. Integer overflow while multiplying LastBlock and BlockSize.
    
    Change-Id: Idd103ba31cffcf407520c5c65a9cb46d71ca2dfa

--- QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
+++ QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
@@ -26,6 +26,43 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "PartitionTableUpdate.h"
 #include "AutoGen.h"
 #include <Library/Board.h>
@@ -195,6 +232,31 @@ STATIC BOOLEAN IsUpdatePartitionAttributes ()
   return FALSE;
 }
 
+UINT64 GetPartitionSize (EFI_BLOCK_IO_PROTOCOL *BlockIo)
+{
+  UINT64 PartitionSize;
+
+  if (!BlockIo) {
+    DEBUG ((EFI_D_ERROR, "Invalid parameter, pleae check BlockIo info!!!\n"));
+    return 0;
+  }
+
+  if (CHECK_ADD64 (BlockIo->Media->LastBlock, 1)) {
+    DEBUG ((EFI_D_ERROR, "Integer overflow while adding LastBlock and 1\n"));
+    return 0;
+  }
+
+  if ((MAX_UINT64 / (BlockIo->Media->LastBlock + 1)) <
+    (UINT64)BlockIo->Media->BlockSize) {
+    DEBUG ((EFI_D_ERROR,
+     "Integer overflow while multiplying LastBlock and BlockSize\n"));
+    return 0;
+  }
+
+  PartitionSize = (BlockIo->Media->LastBlock + 1) * BlockIo->Media->BlockSize;
+  return  PartitionSize;
+}
+
 VOID UpdatePartitionAttributes (UINT32 UpdateType)
 {
   UINT32 BlkSz;
@@ -256,7 +318,10 @@ VOID UpdatePartitionAttributes (UINT32 UpdateType)
     }
 
     BlockIo = BlockIoHandle[0].BlkIo;
-    DeviceDensity = (BlockIo->Media->LastBlock + 1) * BlockIo->Media->BlockSize;
+    DeviceDensity = GetPartitionSize (BlockIo);
+    if (!DeviceDensity) {
+      return;
+    }
     BlkSz = BlockIo->Media->BlockSize;
     PartEntriesblocks = MAX_PARTITION_ENTRIES_SZ / BlkSz;
     MaxGptPartEntrySzBytes = (GPT_HDR_BLOCKS + PartEntriesblocks) * BlkSz;
@@ -975,7 +1040,10 @@ WriteGpt (INT32 Lun, UINT32 Sz, UINT8 *Gpt)
   }
 
   BlockIo = BlockIoHandle[0].BlkIo;
-  DeviceDensity = (BlockIo->Media->LastBlock + 1) * BlockIo->Media->BlockSize;
+  DeviceDensity = GetPartitionSize (BlockIo);
+  if (!DeviceDensity) {
+    return FAILURE;
+  }
   BlkSz = BlockIo->Media->BlockSize;
 
   /* Verity that passed block has valid GPT primary header */
