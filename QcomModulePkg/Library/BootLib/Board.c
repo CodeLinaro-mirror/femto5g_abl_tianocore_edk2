7ccc45bf2a3ba7cbc25734c94da632cfa9ae500d
quic_ysaispur <quic_ysaispur@quicinc.com>
2022-11-14 09:41:06 -0800

    Revert "QcomModulePkg: Move chip serial no checking"
    This reverts commit d14d430ac07ad0c03cab1a77c2f905fc7eb0d819.
    
    Change-Id: Ia2c465f93c9b61100ac9ffce0bfdef984d05a0e6
    Signed-off-by: Spurthy Yadagiri <quic_ysaispur@quicinc.com>

--- QcomModulePkg/Library/BootLib/Board.c
+++ QcomModulePkg/Library/BootLib/Board.c
@@ -627,7 +627,20 @@ BoardSerialNum (CHAR8 *StrSerialNum, UINT32 Len)
   MemCardType Type = EMMC;
 
   Type = CheckRootDeviceType ();
-  if (Type == UNKNOWN) {
+  if (Type == UNKNOWN)
+    return EFI_NOT_FOUND;
+
+  Status = GetDeviceHandleInfo (HandleInfoList, MaxHandles, Type);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Status =
+      gBS->HandleProtocol (HandleInfoList[0].Handle,
+                           &gEfiMemCardInfoProtocolGuid, (VOID **)&CardInfo);
+  if (Status != EFI_SUCCESS) {
+    DEBUG ((EFI_D_ERROR, "Error locating MemCardInfoProtocol:%x\n", Status));
+
     Status = gBS->LocateProtocol (&gEfiChipInfoProtocolGuid, NULL,
                                   (VOID **)&ChipInfo);
     if (Status != EFI_SUCCESS) {
@@ -644,20 +657,6 @@ BoardSerialNum (CHAR8 *StrSerialNum, UINT32 Len)
 
     AsciiSPrint (StrSerialNum, Len, "%x", SerialNo);
     ToLower (StrSerialNum);
-    return Status;
-  };
-
-  Status = GetDeviceHandleInfo (HandleInfoList, MaxHandles, Type);
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  Status =
-      gBS->HandleProtocol (HandleInfoList[0].Handle,
-                           &gEfiMemCardInfoProtocolGuid, (VOID **)&CardInfo);
-  if (Status != EFI_SUCCESS) {
-    DEBUG ((EFI_D_ERROR, "Error locating MemCardInfoProtocol:%x\n", Status));
-
     return Status;
   }
 
