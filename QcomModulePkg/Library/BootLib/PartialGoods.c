98081854ca20a3e1c5c44ad76df734c0d663f296
Naina Mehta <quic_nainmeht@quicinc.com>
2023-01-07 14:33:01 +0530

    QcomModulePkg: PartialGoods: Update logic to disable modem PIL node
    Disable modem PIL node only when all modem, NAV and WLAN are defective.
    
    Change-Id: Ifb16968e271ad11acc6790f96129d365621b01d7

--- QcomModulePkg/Library/BootLib/PartialGoods.c
+++ QcomModulePkg/Library/BootLib/PartialGoods.c
@@ -30,43 +30,7 @@
 /*
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted (subject to the limitations in the
- *  disclaimer below) provided that the following conditions are met:
- *
- *      * Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- *
- *      * Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials provided
- *        with the distribution.
- *
- *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *        contributors may be used to endorse or promote products derived
- *        from this software without specific prior written permission.
- *
- *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- *   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022, 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted (subject to the limitations in the
@@ -375,12 +339,14 @@ static struct PartialGoods PartialGoodsMmType[] = {
     {BIT (EFICHIPINFO_PART_MODEM),
      "/soc",
      {"qcom,mss", "status", "ok", "no"}},
-    {BIT (EFICHIPINFO_PART_MODEM),
+    {(BIT (EFICHIPINFO_PART_MODEM)
+     | BIT (EFICHIPINFO_PART_WLAN)
+     | BIT (EFICHIPINFO_PART_NAV)),
      "/soc",
      {"remoteproc-mss", "status", "ok", "no"}},
     {BIT (EFICHIPINFO_PART_WLAN),
      "/soc",
-     {"qcom,mss", "status", "ok", "no"}},
+     {"qcom,wpss", "status", "ok", "no"}},
     {BIT (EFICHIPINFO_PART_WLAN),
      "/soc",
      {"remoteproc-wpss", "status", "ok", "no"}},
@@ -417,9 +383,6 @@ static struct PartialGoods PartialGoodsMmType[] = {
     {BIT (EFICHIPINFO_PART_NPU),
      "/soc",
      {"qcom,npu", "status", "ok", "no"}},
-    {BIT (EFICHIPINFO_PART_NAV),
-     "/soc",
-     {"qcom,mss", "status", "ok", "no"}},
 };
 
 STATIC EFI_STATUS
@@ -470,6 +433,16 @@ FindNodeAndUpdateProperty (VOID *fdt,
   for (i = 0; i < TableSz; i++, Table++) {
     if (!(Value & Table->Val))
       continue;
+
+    if (Table->Val == (BIT (EFICHIPINFO_PART_MODEM) |
+                       BIT (EFICHIPINFO_PART_WLAN) |
+                       BIT (EFICHIPINFO_PART_NAV))) {
+      if (!((Value & BIT (EFICHIPINFO_PART_MODEM)) &&
+            (Value & BIT (EFICHIPINFO_PART_WLAN)) &&
+            (Value & BIT (EFICHIPINFO_PART_NAV)))) {
+        continue;
+      }
+    }
 
     /* Find the parent node */
     ParentOffset = FdtPathOffset (fdt, Table->ParentNode);
