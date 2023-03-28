31695ada0edcf0b80ef5297b900e738f29d48081
Laszlo Ersek <lersek@redhat.com>
2023-03-28 13:26:24 +0530

    SecurityPkg/DxeImageVerificationLib: extract SecDataDirEnd, SecDataDirLeft
    The following two quantities:
    
      SecDataDir->VirtualAddress + SecDataDir->Size
      SecDataDir->VirtualAddress + SecDataDir->Size - OffSet
    
    are used multiple times in DxeImageVerificationHandler(). Introduce helper
    variables for them: "SecDataDirEnd" and "SecDataDirLeft", respectively.
    This saves us multiple calculations and significantly simplifies the code.
    
    Note that all three summands above have type UINT32, therefore the new
    variables are also of type UINT32.
    
    This patch does not change behavior.
    
    (Note that the code already handles the case when the
    
      SecDataDir->VirtualAddress + SecDataDir->Size
    
    UINT32 addition overflows -- namely, in that case, the certificate loop is
    never entered, and the corruption check right after the loop fires.)
    
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Min Xu <min.m.xu@intel.com>
    Cc: Wenyi Xie <xiewenyi2@huawei.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=2215
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20200901091221.20948-2-lersek@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Tested-by: Wenyi Xie <xiewenyi2@huawei.com>
    Reviewed-by: Min M Xu <min.m.xu@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>
    Git-commit: 503248ccdf45c14d4040ce44163facdc212e4991
    Git-repo: https://github.com/tianocore/edk2.git
    
    Change-Id: I4f4ef5e49dcd56e474fff009b485298d0dfaf3eb

--- SecurityPkg/Library/DxeImageVerificationLib/DxeImageVerificationLib.c
+++ SecurityPkg/Library/DxeImageVerificationLib/DxeImageVerificationLib.c
@@ -1567,6 +1567,8 @@ DxeImageVerificationHandler (
   UINT8                                *AuthData;
   UINTN                                AuthDataSize;
   EFI_IMAGE_DATA_DIRECTORY             *SecDataDir;
+  UINT32                               SecDataDirEnd;
+  UINT32                               SecDataDirLeft;
   UINT32                               OffSet;
   CHAR16                               *NameStr;
 
@@ -1757,12 +1759,14 @@ DxeImageVerificationHandler (
   // "Attribute Certificate Table".
   // The first certificate starts at offset (SecDataDir->VirtualAddress) from the start of the file.
   //
+  SecDataDirEnd = SecDataDir->VirtualAddress + SecDataDir->Size;
   for (OffSet = SecDataDir->VirtualAddress;
-       OffSet < (SecDataDir->VirtualAddress + SecDataDir->Size);
+       OffSet < SecDataDirEnd;
        OffSet += (WinCertificate->dwLength + ALIGN_SIZE (WinCertificate->dwLength))) {
     WinCertificate = (WIN_CERTIFICATE *) (mImageBase + OffSet);
-    if ((SecDataDir->VirtualAddress + SecDataDir->Size - OffSet) <= sizeof (WIN_CERTIFICATE) ||
-        (SecDataDir->VirtualAddress + SecDataDir->Size - OffSet) < WinCertificate->dwLength) {
+    SecDataDirLeft = SecDataDirEnd - OffSet;
+    if (SecDataDirLeft <= sizeof (WIN_CERTIFICATE) ||
+        SecDataDirLeft < WinCertificate->dwLength) {
       break;
     }
 
@@ -1837,7 +1841,7 @@ DxeImageVerificationHandler (
     }
   }
 
-  if (OffSet != (SecDataDir->VirtualAddress + SecDataDir->Size)) {
+  if (OffSet != SecDataDirEnd) {
     //
     // The Size in Certificate Table or the attribute certicate table is corrupted.
     //
