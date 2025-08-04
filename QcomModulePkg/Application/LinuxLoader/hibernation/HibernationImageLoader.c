9e27e8125a23c819f39b751791a03243aa468adc
Shubham Chouhan <schouhan@qti.qualcomm.com>
2025-08-03 23:45:56 -0700

    Revert "Enable 4 cores for Decompression/Decryption"
    This reverts commit a6af8fdad91063155ae5fb42e3d112d8e5a8c328.
    
    Reason for revert: For validation
    
    Change-Id: Ic9e7e6d8805193829567ad6f8fea093ccdf7df66

--- QcomModulePkg/Application/LinuxLoader/hibernation/HibernationImageLoader.c
+++ QcomModulePkg/Application/LinuxLoader/hibernation/HibernationImageLoader.c
@@ -99,10 +99,10 @@ typedef struct FreeRanges {
 }FreeRanges;
 
 #if HIBERNATION_SUPPORT_AES
-#define NUM_CORES 8
-#define NUM_SILVER_CORES 4
-#define NUM_PAGES_PER_GOLD_CORE ((NrCopyPages / 54) * 9)
-#define NUM_PAGES_PER_SILVER_CORE ((NrCopyPages / 54) * 4)
+#define NUM_CORES 2
+#define NUM_SILVER_CORES 1
+#define NUM_PAGES_PER_GOLD_CORE 0
+#define NUM_PAGES_PER_SILVER_CORE 0
 
 static struct DecryptParam *Dp;
 static VOID *Authslot;
