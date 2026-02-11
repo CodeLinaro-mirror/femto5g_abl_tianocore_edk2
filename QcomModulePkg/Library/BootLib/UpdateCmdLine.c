ebc11145c2dc8d1b702f2e3013da159165bc8030
Yadu MG <ymg@qti.qualcomm.com>
2026-02-12 00:23:41 +0530

    QcomModulePkg: Harden audio-framework oem command handling
    Add allowlist validation and harden fastboot/devinfo reads/writes for
    audio-framework oem fastboot command.
    
    Change-Id: Id6679530e1bbc696155c3435ec84e6d21e12aa43
    (cherry picked from commit a83afbadcdb40500ba042fd4dbd028a4992f9777)

--- QcomModulePkg/Library/BootLib/UpdateCmdLine.c
+++ QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -31,35 +31,9 @@
  *
  **/
 /*
-  * Changes from Qualcomm Innovation Center are provided under the following
-  * license:
-  * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted (subject to the limitations in the disclaimer
-  * below) provided that the following conditions are met:
-  *  * Redistributions of source code must retain the above copyright notice,
-  *    this list of conditions and the following disclaimer.
-  *  * Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided ?with the distribution.
-  *  * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
-  *     contributors may be used to endorse or promote products derived from this
-  *     software without specific prior written permission.
-  *
-  * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED
-  * BY THIS LICENSE.
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-  * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
   */
 
 
@@ -431,25 +405,38 @@ STATIC EFI_STATUS GetGpuCmdline (VOID)
   return Status;
 }
 
-
 STATIC VOID
-GetAudioFrameWork (CHAR8 *FrameWork, UINT32* Length)
+GetAudioFrameWork (IN OUT CHAR8 *FrameWork, IN OUT UINT32 *Length)
 {
   EFI_STATUS Status;
-  CHAR8 *Src;
-  CHAR8 *AUDIOFRAMEWORK;
+  CHAR8      *Src;
+  UINTN       SrcLen;
 
-  AUDIOFRAMEWORK = GetAudioFw ();
-
-  if ((*Length = AsciiStrLen (AUDIOFRAMEWORK)) > 0) {
-      AsciiStrCpyS (FrameWork, *Length + 1, AUDIOFRAMEWORK);
-      Status = ReadAudioFrameWork (&Src, Length);
-    if (Status == EFI_SUCCESS) {
-      if (*Length) {
-        AsciiStrCpyS (FrameWork, *Length, Src);
-      }
-    }
+  if (FrameWork == NULL || Length == NULL) {
+    return;
   }
+
+  gBS->SetMem (FrameWork, MAX_AUDIO_FW_LENGTH, 0);
+
+  Status = ReadAudioFrameWork (&Src, Length);
+  if (EFI_ERROR (Status) || Src == NULL) {
+    *Length = 0;
+    return;
+  }
+
+  SrcLen = AsciiStrnLenS (Src, MAX_AUDIO_FW_LENGTH);
+  if (SrcLen == 0 || SrcLen >= MAX_AUDIO_FW_LENGTH) {
+    *Length = 0;
+    return;
+  }
+
+  if (!IsAllowedAudioFramework (Src)) {
+    *Length = 0;
+    return;
+  }
+
+  AsciiStrnCpyS (FrameWork, MAX_AUDIO_FW_LENGTH, Src, SrcLen);
+  *Length = (UINT32)AsciiStrnLenS (FrameWork, MAX_AUDIO_FW_LENGTH);
 }
 
 /*
